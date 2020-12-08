19768277ff9247ebb42f2a31c29c6f8c775131bb
critiqalfish <69545904+critiqalfish@users.noreply.github.com>
2020-12-08 15:33:17 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,5 +2,10 @@ const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
+});
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
 	document.hr.classList.toggle('dark');
 });
